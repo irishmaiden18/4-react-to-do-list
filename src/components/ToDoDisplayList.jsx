eb46cdd476180023865391a8c760acb1544ea019
irishmaiden18 <laurapohl18@gmail.com>
2026-04-24 12:24:25 -0400

    fixes formatting issue

--- src/components/ToDoDisplayList.jsx
+++ src/components/ToDoDisplayList.jsx
@@ -15,19 +15,25 @@ const ToDoDisplayList = ({toDos, deleteToDo, deleteCompletedToDos, displayUserna
             // filter all the toDos with the given displayUsername and add them to the copied array
             updatedToDos = toDos.filter((task) => task.username === displayUsername)
         }
-        
+    
+
+
         // function return
         return (
             <>
                 {/* if updatedToDos has no elements */}
                 {/* cycle through the updatedToDos to display each one below as an LI using the ToDoDisplay Item component. feed necessary props */}
                 {updatedToDos.length !== 0 ?
-                    (updatedToDos.map((toDo) => (
-                        // set key equal to the unique id's created when adding a toDo
-                        <li key={toDo.id}>
-                            <ToDoDisplayItem toDo={toDo} deleteToDo={deleteToDo} editTodo={editTodo} handleCompleteTask={handleCompleteTask}/>
-                        </li>
-                    ))) : (
+                    (
+                        <ol>
+                            {updatedToDos.map((toDo) => (
+                            // set key equal to the unique id's created when adding a toDo
+                            <li key={toDo.id}>
+                                <ToDoDisplayItem toDo={toDo} deleteToDo={deleteToDo} editTodo={editTodo} handleCompleteTask={handleCompleteTask}/>
+                            </li>
+                            ))}   
+                        </ol>
+                    ) : (
                         <h3 className="empty-list">List is Empty! Please add an item.</h3>
                     )}
             </>
@@ -53,9 +59,8 @@ const ToDoDisplayList = ({toDos, deleteToDo, deleteCompletedToDos, displayUserna
 
         ) : (
 
-            <ol>
-                {displayList(toDos)}
-            </ol>
+            displayList(toDos)
+           
         )}
 
 
